5080544ebdaf92aa2b2da3ff525c0e1950b5f268
Yurberto <sta1993lone@gmail.com>
2025-10-04 14:37:24 +0300

    Changed shooter logic
    Vector 2 _offset ==> Transfrom _spawnpoint

--- Assets/Scripts/Shooting/Shooter.cs
+++ Assets/Scripts/Shooting/Shooter.cs
@@ -3,20 +3,13 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField, Range(0.0f, 200.0f)] private float _missileSpeed = 5.0f;
-    [SerializeField] private Vector2 _spawnOffset;
+    [SerializeField] private Transform _spawnpoint;
 
     private MissileSpawner _missileSpawner;
-
-    private Vector3 _spawnOffset3D;
 
     public void Initialize(MissileSpawner missileSpawner)
     {
         _missileSpawner = missileSpawner;
-    }
-
-    private void Awake()
-    {
-        _spawnOffset3D = new Vector3(_spawnOffset.x, _spawnOffset.y, 0.0f);
     }
 
     public void Shoot(Vector2 direction)
@@ -26,7 +19,7 @@ public class Shooter : MonoBehaviour
 
         Missile spawnedMissile = _missileSpawner.Spawn();
 
-        spawnedMissile.transform.position = transform.position + _spawnOffset3D;
+        spawnedMissile.transform.position = _spawnpoint.position;
         spawnedMissile.Rigidbody2D.velocity = direction * _missileSpeed;
     }
 }
