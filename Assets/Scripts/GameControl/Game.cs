bb391c9b770515c373b361d42b824ee41ccabbb8
Yurberto <sta1993lone@gmail.com>
2025-10-04 12:46:05 +0300

    /

---
+++ Assets/Scripts/GameControl/Game.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _player.Dead += Stop;
+    }
+
+    private void OnDisable()
+    {
+        _player.Dead -= Stop;
+    }
+
+    private void Stop()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void Play()
+    {
+        Time.timeScale = 1;
+    }
+}
